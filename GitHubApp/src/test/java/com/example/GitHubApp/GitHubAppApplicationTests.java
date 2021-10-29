8e2d51fced2e62c4db2b08e88899f4d75cc454ef
KallanKhan <59261357+KallanKhan@users.noreply.github.com>
2021-10-30 00:07:40 +0530

    commit all file

---
+++ GitHubApp/src/test/java/com/example/GitHubApp/GitHubAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitHubApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
